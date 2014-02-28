de48b8b9c6a9a542cc0bf9bb140a169879794369
neodevelop <neodevelop@gmail.com>
2014-02-28 15:28:39 -0600

    implementing controller with uri templates

--- web/src/main/java/com/makingdevs/practica3/UserStoryController.java
+++ web/src/main/java/com/makingdevs/practica3/UserStoryController.java
@@ -1,11 +1,14 @@
 package com.makingdevs.practica3;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.makingdevs.model.UserStory;
 import com.makingdevs.services.UserStoryService;
 
 @Controller
@@ -16,7 +19,10 @@ public class UserStoryController {
 
   @RequestMapping("/project/{codeName}/userStories")
   public String allProjects(@PathVariable("codeName") String codeName, Model model) {
-    
+    List<UserStory> userStories = userStoryService.findUserStoriesByProject(codeName);
+    model.addAttribute("project",userStories.get(0).getProject());
+    // Hey ma! help me to validate..
+    model.addAttribute("userStories",userStories);
     return "userStory/project";
   }
 
