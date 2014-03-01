33da5157f2bef774d221def941a8302599dd8164
neodevelop <neodevelop@gmail.com>
2014-02-28 21:43:09 -0600

    agregando la forma de búsqueda

--- web/src/main/java/com/makingdevs/practica5/SearchController.java
+++ web/src/main/java/com/makingdevs/practica5/SearchController.java
@@ -1,20 +1,45 @@
 package com.makingdevs.practica5;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.makingdevs.model.Project;
+import com.makingdevs.model.Task;
+import com.makingdevs.repositories.ProjectRepository;
+import com.makingdevs.repositories.TaskRepository;
 
 @Controller
-@RequestMapping(value="/search/**")
+@RequestMapping(value = "/search/**")
 public class SearchController {
   
-  @RequestMapping(method=RequestMethod.GET)
-  public String searchInProjects(){
-    return null;
-  }
+  @Autowired
+  ProjectRepository projectRepository;
   
-  @RequestMapping(method=RequestMethod.POST)
-  public String searchResults(){
-    return null;
+  @Autowired
+  TaskRepository taskRepository;
+
+  @RequestMapping(method = RequestMethod.GET)
+  public void searchInProjects() {
+
+  }
+
+  @RequestMapping(method = RequestMethod.POST)
+  public Map<String,Object> searchResults(@RequestParam("codeName") String codeName,
+      @RequestParam("taskDescription") String taskDescription) {
+    Map<String,Object> model = new HashMap<String,Object>();
+    Project project = projectRepository.findByCodeName(codeName);
+    List<Task> tasks = taskRepository.findAllByDescriptionLike(taskDescription);
+    model.put("codeName",codeName);
+    model.put("taskDescription",taskDescription);
+    model.put("project", project);
+    model.put("tasks", tasks);
+    return model;
   }
 }
--- web/src/main/webapp/WEB-INF/jsp/search.jsp
+++ web/src/main/webapp/WEB-INF/jsp/search.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
   pageEncoding="UTF-8"%>
 <!DOCTYPE html>
@@ -14,8 +15,34 @@
   </div>
 
   <div class="container">
-    
-    
+    <form action="${pageContext.request.contextPath}/search" method="post">
+      <div class="form-group">
+        <label for="name">Project code</label>
+        <input name="codeName" placeholder="Code Name" class="form-control" value="${codeName}">
+      </div>
+      <div class="form-group">
+        <label for="name">Task description</label>
+        <input name="taskDescription" placeholder="What did you write?" class="form-control" value="${taskDescription}">
+      </div>
+      <button type="submit" class="btn btn-default">Search in projects</button>
+    </form>
+  </div>
+  <hr>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <h3>Projects <small></small></h3>
+        
+      </div>
+      <div class="col-md-6">
+        <h3>Tasks</h3>
+        <ul>
+        <c:forEach items="${tasks}" var="task">
+          <li>${task.description}</li>
+        </c:forEach>
+        </ul> 
+      </div>
+    </div>
   </div>
 
   <jsp:include page="_footer.jsp" />
