2b380f59881a2e89eda462408a279b64fbe6781e
neodevelop <neodevelop@gmail.com>
2014-02-28 21:18:23 -0600

    agregando métodos de finders

--- repositoriesAndServices/src/main/java/com/makingdevs/repositories/TaskRepository.java
+++ repositoriesAndServices/src/main/java/com/makingdevs/repositories/TaskRepository.java
@@ -1,12 +1,16 @@
 package com.makingdevs.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.makingdevs.model.Task;
+import com.makingdevs.model.TaskStatus;
+import com.makingdevs.model.User;
+import com.makingdevs.model.UserStory;
 
 public interface TaskRepository extends CrudRepository<Task, Long> {
-  // List<Task> findAllByDescriptionLike(String description);
-  // List<Task> findAllByUserStoryAndStatusEquals(UserStory userStory,
-  // TaskStatus taskStatus);
-  // List<Task> findAllByUser(User user);
+  List<Task> findAllByDescriptionLike(String description);
+  List<Task> findAllByUserStoryAndStatusEquals(UserStory userStory,TaskStatus taskStatus);
+  List<Task> findAllByUser(User user);
 }
